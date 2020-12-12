53e81d8955629a2478cb256d7f938bcfc9dcd96d
Nezar <hamideh@yandex.ru>
2020-12-12 20:28:49 +0300

    cube and viewer fixes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,66 +1,102 @@
-let viewer;
-const documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDIwLTEwLTE5LTE5LTI0LTA0LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlLyVEMCU5NyVEMCVCMCVEMCVCMSVEMCVCRSVEMSU4MCUyMCVEMCU5NiVEMCVCMCVEMCVCQiVEMSU4RSVEMCVCNyVEMCVCOCUyMHYzNC5mM2Q'
-const options = {
+
+/* const options = {
     env: 'AutodeskProduction',
     api: 'derivativeV2',  // for models uploaded to EMEA change this option to 'derivativeV2_EU'
     getAccessToken: getForgeToken,
-}
+} */
+$("#viewer").click(function () {
+    NOP_VIEWER.getProperties(NOP_VIEWER.getSelection(), data => console.log(data))
+});
+let viewer;
+const documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6cGFjbWFuLWZvcmdlL2N1YmUuaXB0';
 
+$(document).ready(function () {
+    launchViewer();
+});
 
 function getForgeToken(onTokenReady) {
     $.get("/oauth", (data) => {
         const token = data.access_token;
-        const timeInSeconds = data.expires_in; // Use value provided by Forge Authentication (OAuth) API
+        const timeInSeconds = data.expires_in;
         onTokenReady(token, timeInSeconds);
     });
 }
 
-Autodesk.Viewing.Initializer(options, function () {
-    loadModel();
-});
-
-function loadModel() {
-    const htmlDiv = document.getElementById('viewer');
+function launchViewer() {
+    var options = {
+        env: 'AutodeskProduction',
+        getAccessToken: getForgeToken
+    };
     const config = {
-        disabledExtensions: {
+        extensions: ['Autodesk.ViewCubeUi']
+        /* disabledExtensions: {
             measure: true
-        }
+        } */
     };
 
-    viewer = new Autodesk.Viewing.GuiViewer3D(htmlDiv, config);
-    const startedCode = viewer.start();
-    if (startedCode > 0) {
-        console.error('Failed to create a Viewer: WebGL not supported.');
-        return;
-    }
-
-    console.log('Initialization complete, loading a model next...');
-    viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, (e) => {
-        viewer.setLightPreset(8);
-    })
-
-    viewer.addEventListener(Autodesk.Viewing.TOOLBAR_CREATED_EVENT, onToolBarCreated)
-
-    Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
-}
-
-function onLoadModelSuccess(model) {
-    viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, (e) => {
-        viewer.setLightPreset(8);
+    Autodesk.Viewing.Initializer(options, () => {
+        viewer = new Autodesk.Viewing.Viewer3D(document.getElementById('viewer'), config);
+        viewer.start();
+        //viewer.addEventListener(Autodesk.Viewing.TOOLBAR_CREATED_EVENT, onToolBarCreated)
+        viewer.addEventListener(Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT, function () {
+            const tree = viewer.model.getInstanceTree();
+            const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
+            material.side = THREE.DoubleSide;
+            const materialManager = viewer.impl.matman();
+            materialManager.addMaterial('myCustomMaterial', material, true);
+            const model = viewer.model;
+            model.unconsolidate(); // If the model is consolidated, material changes won't have any effect
+            const frags = model.getFragmentList();
+            const dbids = [0, 3];
+            for (const dbid of dbids) {
+                tree.enumNodeFragments(dbid, (fragid) => {
+                    frags.setMaterial(fragid, material);
+                });
+            }
+            viewer.impl.invalidate(true);
+        });
+        Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
     });
 }
 
-function onLoadModelError(viewerErrorCode) {
-    console.error('onLoadModelError() - errorCode:' + viewerErrorCode);
-}
-
 function onDocumentLoadSuccess(doc) {
-    const defaultModel = doc.getRoot().getDefaultGeometry();
-    viewer.loadDocumentNode(doc, defaultModel);
+    var viewables = doc.getRoot().getDefaultGeometry();
+    viewer.loadDocumentNode(doc, viewables).then(i => {
+        let cubeExt = viewer.getExtension('Autodesk.ViewCubeUi');
+        cubeExt.setViewCube('front');
+        //cubeExt.displayViewCube(false);
+        cubeExt.setVisible(false);
+
+        viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, (e) => {
+            viewer.setBackgroundColor(40, 40, 40, 40, 40, 40); //fix
+            viewer.setQualityLevel(true, true);
+            viewer.setGhosting(true);
+            viewer.setGroundShadow(false);
+            viewer.setGroundReflection(false);
+            viewer.setProgressiveRendering(true);
+
+            disableEventsEvents();
+        });
+    });
 }
 
-function onDocumentLoadFailure() {
-    console.error('Failed fetching Forge manifest');
+function disableEventsEvents() {
+    let config = {
+        "click": {
+            "onObject": ['selectToggle'],
+            "offObject": ['deselectAll']
+        },
+        "disableMouseWheel": true
+    };
+    viewer.setCanvasClickBehavior(config);
+    viewer.clickHandler.handleDoubleClick = (e) => {
+        return;
+    }
+    //viewer.toolController.deactivateTool('orbit');
+}
+
+function onDocumentLoadFailure(viewerErrorCode) {
+    console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode);
 }
 
 function onToolBarCreated() {
@@ -71,5 +107,3 @@ function onToolBarCreated() {
         }
     });
 }
-
-
